4d7280ebf91073741f916d74f943a1586dadf410
Z0rb14n <babybluewoof@gmail.com>
2023-02-20 21:28:14 -0800

    Tomfoolery

---
+++ Assets/Scripts/TranslationUI/TranslationPuzzle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SymbolBook;
+using UnityEngine;
+
+namespace TranslationUI
+{
+    /// <summary>
+    ///     Represents an item.
+    /// </summary>
+    [CreateAssetMenu(fileName = "NewTranslationPuzzle", menuName = "Translation/Puzzle")]
+    public class TranslationPuzzle : ScriptableObject
+    {
+        [Tooltip("Object Name")]
+        public string objectName = "";
+        [Tooltip("Words")]
+        public WordPair[] words;
+
+        [Serializable]
+        public struct WordPair
+        {
+            [Tooltip("Word")]
+            public Symbol word;
+            [Tooltip("Possible options")]
+            public string[] options;
+            [Tooltip("Hint, empty/null means none")]
+            public string hint;
+            [Tooltip("Whether this has a fixed hint")]
+            public bool startKnown;
+        }
+    }
+}
