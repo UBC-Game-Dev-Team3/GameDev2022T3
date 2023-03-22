522a19d17d127cd2ebcd4ffd0c120898dae28999
Z0rb14n <babybluewoof@gmail.com>
2023-03-21 21:35:43 -0700

    TutorialUI, why is there meowing

---
+++ Assets/Scripts/TutorialUI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using Util;
+
+public class TutorialUI : MonoBehaviour
+{
+    private void Awake()
+    {
+        PlayerRelated.TriggerUIOpen();
+    }
+
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void OnButtonPress()
+    {
+        PlayerRelated.TriggerUIClose();
+        Destroy(gameObject);
+    }
+}
